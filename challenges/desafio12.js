f7f08f7e7c4b3a9f4895fdc85537c45be2b209c1
joanamds <joanamdsantos1@gmail.com>
2023-04-18 10:44:48 -0300

    :bug: fix: consertando o requisito 12

--- challenges/desafio12.js
+++ challenges/desafio12.js
@@ -1,4 +1,9 @@
 db.produtos.updateMany(
   { nome: { $ne: "McChicken" } },
-  { $addToSet: { ingredientes: "Ketchup" } },
+  { $addToSet: { ingredientes: "ketchup" } },
+);
+
+db.produtos.find(
+  {}, 
+  { nome: 1, ingredientes: 1, _id: 0 },
 );
